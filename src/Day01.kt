62170a2598946db33f94a08dd43c53297df39bec
Audrey <audreysfwilliams@gmail.com>
2022-12-06 15:34:26 +0000

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,13 +1,30 @@
 fun main() {
     val input = readInput("Day01")
+    var max: Int = Int.MIN_VALUE
     val elves = mutableListOf<Int>()
     var sum = 0
-    input.forEach() {
-        if (it.isNotEmpty()) sum += it.toInt()
-        else {
-            elves.add(sum)
-            sum = 0
+
+    fun part1(input: List<String>): Int {
+        input.forEach() {
+            if (it.isNotEmpty()) sum += it.toInt()
+            else {
+                if (sum > max) max = sum
+                sum = 0
+            }
         }
+        return max
     }
-    println(elves.sortedDescending().take(3).sum())
+
+    fun part2(input: List<String>): Int {
+        input.forEach() {
+            if (it.isNotEmpty()) sum += it.toInt()
+            else {
+                elves.add(sum)
+                sum = 0
+            }
+        }
+        return elves.sortedDescending().take(3).sum()
+    }
+    println(part1(input))
+    println(part2(input))
 }
